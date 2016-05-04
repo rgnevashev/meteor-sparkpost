ea2d353b0f22dbee02bcca383db28491af283e89
Roman <rgnevashev@gmail.com>
2016-05-04 12:23:25 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rgnevashev:sparkpost',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'A Node.js wrapper for interfacing with your favorite SparkPost APIs',
   git: 'https://github.com/rgnevashev/meteor-sparkpost.git',
   documentation: 'README.md'
